8de0634dcfb49e0fb5122a1e0740b1291742850b
theorallon <theo_rallon@estudante.sesisenai.org.br>
2025-09-14 17:45:05 -0300

    Lixeira para deletar itens da lista

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ function adicionarTarefa() {
             // Cria a variável "tarefa" e atribui a ela o valor digitado no input
             let inputTarefa = document.getElementById("inputTarefa");
 
+            let body = document.getElementById("body");
+
             //Cria a variável "tarefa", e denomina seu valor como o valor do "inputTarefa" para podermos ver o que tem dentro do input
             let tarefa = inputTarefa.value.trim();
 
@@ -28,6 +30,22 @@ function adicionarTarefa() {
 
             //faz com que o <li> entre no <ul> e vire um elemento filho (respeitando a hieraquia do HTML)
             listaTarefas.appendChild(novaTarefa);
+
+            //criando icone da lixeira
+            let icone = document.createElement("ion-icon")
+            //atribuindo o icone lixeira a tag icone
+            icone.setAttribute("name", "trash-bin-outline");
+
+            //conectando ela ao item da lista
+            novaTarefa.appendChild(icone)
+
+            icone.addEventListener('click', excluirItem)
+
+             // função para remover item da lista
+            function excluirItem(){
+            novaTarefa.remove()
+            
+            }
 
             // Limpa o campo de entrada após adicionar a tarefa
             inputTarefa.value = ""
